d22d4177c2c29d8f49750c48861dbede843e4ba9
Ariel Barrios <ariel29111978@hotmail.com>
2023-12-19 17:21:40 -0300

    Agrega la funsionalidad para manejar las issues

--- src/wh-github/wh-github.service.ts
+++ src/wh-github/wh-github.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import {
   GitHubEvent,
+  GitHubIssue,
   GitHubPayload,
   GitHubStar,
 } from 'src/interfaces/wh-github.interface';
@@ -15,6 +16,10 @@ export class WhGithubService {
         message = this.handleStar(payload as GitHubStar);
         break;
 
+      case 'issues':
+        message = this.handleIssue(payload as GitHubIssue);
+        break;
+
       default:
         message = `unknown event ${event}`;
         break;
@@ -26,5 +31,23 @@ export class WhGithubService {
   private handleStar(payload: GitHubPayload) {
     const { action, sender, repository } = payload;
     return `User ${sender.login} ${action} star on ${repository.full_name}`;
+  }
+
+  private handleIssue(payload: GitHubIssue): string {
+    const { action, issue, sender } = payload;
+
+    if (action === 'opened') {
+      return `An issue was opened with this title ${issue.title} by ${sender.login}`;
+    }
+
+    if (action === 'closed') {
+      return `An issue was closed by ${sender.login}`;
+    }
+
+    if (action === 'reopened') {
+      return `An issue was reopened by ${sender.login}`;
+    }
+
+    return `Unhandle action for the issue event ${action}`;
   }
 }
