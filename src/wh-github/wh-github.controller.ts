d6e5dc71a21848fabfb844e11a39353f8c97619c
Ariel Barrios <ariel29111978@hotmail.com>
2023-12-19 17:05:47 -0300

    Agrega la función handlePayload yhandleStar

--- src/wh-github/wh-github.controller.ts
+++ src/wh-github/wh-github.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Headers, Post } from '@nestjs/common';
 import { WhGithubService } from './wh-github.service';
+import { GitHubEvent } from 'src/interfaces/wh-github.interface';
 
 @Controller('wh-github')
 export class WhGithubController {
@@ -7,10 +8,11 @@ export class WhGithubController {
 
   @Post()
   webhookHandler(
-    @Headers('x-github-event') githubEvent: string,
+    @Headers('x-github-event') githubEvent: GitHubEvent,
     @Body() body: any,
   ) {
-    console.log({ githubEvent });
+    // console.log({ githubEvent });
+    this.whGithubService.handlePayload(githubEvent, body);
     return { githubEvent };
   }
 }
--- src/wh-github/wh-github.service.ts
+++ src/wh-github/wh-github.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import {
+  GitHubEvent,
+  GitHubPayload,
+  GitHubStar,
+} from 'src/interfaces/wh-github.interface';
 
 @Injectable()
-export class WhGithubService {}
+export class WhGithubService {
+  public handlePayload(event: GitHubEvent, payload: GitHubPayload) {
+    let message = '';
+
+    switch (event) {
+      case 'star':
+        message = this.handleStar(payload as GitHubStar);
+        break;
+
+      default:
+        message = `unknown event ${event}`;
+        break;
+    }
+
+    console.log(message);
+  }
+
+  private handleStar(payload: GitHubPayload) {
+    const { action, sender, repository } = payload;
+    return `User ${sender.login} ${action} star on ${repository.full_name}`;
+  }
+}
